431922e03f11a50815499960c33405576d6b2828
RAKSHITH S B <70064036+SB-Rakshith@users.noreply.github.com>
2023-08-28 17:16:59 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -10,73 +10,60 @@ $('.menu-toggle').click(function(){
 });
 
 
-// -----------------------------------header image start--------------------------------- //
-
-
-
-// Function to convert the Google Drive URL to the desired format
-function convertDriveURL(url) {
-  const fileId = url.match(/\/file\/d\/(.+?)\/view/)[1];
-  return `https://drive.google.com/uc?export=view&id=${fileId}`;
-}
-
-// Function to fetch the image URL from Google Sheets
-async function fetchImageURL() {
+// ----------------------------------- Fetch Banner Img URL --------------------------------- //
+async function fetchImageURLsAndUpdateImages() {
   const apiKey = 'AIzaSyCxMZ_L50qNssIKZhPTWhbCPQYoDkbP8Es';
   const sheetId = '1hMf8jd8vvjvzvniDNwKa3pyWiOhMsuE7tnSHjwV4d48';
   const sheetName = 'Header Image';
-  let desktopCell;
-  let mobileCell;
 
-  // Check if user is in mobile screen
-  if (window.innerWidth < 768) {
-    // Use B column for mobile
-    mobileCell = 'B2';
-  } else {
-    // Use A column for desktop
-    desktopCell = 'A2';
-  }
+  const sheetURL = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}?key=${apiKey}`;
 
-  const sheetURL = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${sheetName}!${desktopCell || mobileCell}?key=${apiKey}`;
-
-  try {
+ try {
     const response = await fetch(sheetURL);
     const data = await response.json();
-    const imageURLs = data.values[0];
 
-    let currentImageIndex = 0;
-    const section = document.getElementById('headline-section');
+    // Assuming your Google Sheet has a single column of image URLs
+    const imageURLs = data.values.map(row => row[0]);
 
-    // Function to update the background image
-    function updateBackgroundImage() {
-      // Convert the Google Drive URL to the desired format
-      const convertedURL = convertDriveURL(imageURLs[currentImageIndex]);
+    // Get all the existing img elements
+    const imgElements = document.querySelectorAll('#banner-slidder img');
+    let loadedImageCount = 0;
 
-      // Set the converted URL as the background image of the section
-      section.style.backgroundImage = `url(${convertedURL})`;
+    // Function to track when individual images have loaded
+    function onImageLoad() {
+      loadedImageCount++;
 
-      // Increment the current image index
-      currentImageIndex = (currentImageIndex + 1) % imageURLs.length;
+      // Check if all images have loaded
+      if (loadedImageCount === imgElements.length) {
+        // All images have loaded, hide the loader
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('banner-slidder').style.display = 'block';
+
+      }
     }
 
-    // Update the background image initially
-    updateBackgroundImage();
+    // Update the src attribute of each img element
+    imgElements.forEach((img, index) => {
+      if (index < imageURLs.length) {
+        img.src = imageURLs[index];
+        img.alt = 'Image'; // You can set an alt text if needed
 
-    // Set a timer interval to update the background image every 3 seconds
-    setInterval(updateBackgroundImage, 3000);
+        // Attach an onload event handler to each image
+        img.onload = onImageLoad;
+      }
+    });
   } catch (error) {
-    console.error('Error fetching image URL:', error);
+    console.error('Error fetching image URLs:', error);
   }
 }
 
-// Call the function to fetch the image URL and set the background image
-fetchImageURL();
-
-
-
+// Call the function to fetch image URLs and update the existing images
+fetchImageURLsAndUpdateImages();
 // ----------------------------------------------Header image ends ---------------------------------------------//
 
-// ----------------------------------------------Sketch image starts ---------------------------------------------//
+
+
+// ----------------------------------------------Sketch Gallary starts ---------------------------------------------//
 // URL of your Google Sheet (replace with your sheet's ID)
 const googleSheetId = "1hMf8jd8vvjvzvniDNwKa3pyWiOhMsuE7tnSHjwV4d48";
 
@@ -99,7 +86,6 @@ function convertDriveURL(url) {
     }
 }
 
-
 // Function to fetch data from the Google Sheet using the API
 async function fetchData() {
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${googleSheetId}/values/${sheetName}!A2:A?key=${apiKey}`;
@@ -111,21 +97,27 @@ async function fetchData() {
     // Extract image URLs from the API response and convert them
     const imageURLs = data.values.flat().map(convertDriveURL);
 
-    // Display the images on the webpage
-    const imageContainer = document.getElementById("image-container");
-    imageURLs.forEach(url => {
-      const img = document.createElement("img");
-      img.src = url;
-      img.classList.add("image");
-      imageContainer.appendChild(img);
-    });
+    // Update the src attributes of the img elements based on their class names
+    for (let i = 0; i < imageURLs.length; i++) {
+      const className = `image${i + 1}`;
+      const imgElement = document.querySelector(`.${className} img`);
+      
+      if (imgElement) {
+        imgElement.src = imageURLs[i];
+      }
+    }
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
 
+
 // Call the fetchData function to populate the webpage with converted images
 fetchData();
 
-// ----------------------------------------------Sketch image ends ---------------------------------------------//
+// ----------------------------------------------Sketch gallery ends ---------------------------------------------//
+
+//------------------------------------------------Reviews-----------------------------------------------------------//
+// Add this script to your HTML file, preferably just before the closing </body> tag
+
 
